10535c8bc13ceea3387fd2023fb2b05ef3dbe548
eldar-code <eldar.codementor@gmail.com>
2026-03-16 21:05:33 +0200

    add data layer as database

--- 2026-03-09-students-app/app/service.py
+++ 2026-03-09-students-app/app/service.py
@@ -1,44 +1,21 @@
-# data layer
-students = [
-    {"id": 1, "name": "John Doe", "age": 20},
-    {"id": 2, "name": "Jane Smith", "age": 22},
-]
-
-next_id = 3
-
-
 # application logic
+import db
 
 def add_student(student):
-    global next_id
-    student["id"] = next_id
-    next_id += 1
-    students.append(student)
-    return student
+    return db.add_student(student)
 
 
 def get_students():
-    return students
+    return db.get_students()
 
 
 def get_student(student_id):
-    for student in students:
-        if student["id"] == student_id:
-            return student
-    raise KeyError(f"student not found: {student_id}")
+    return db.get_student(student_id)
 
 
 def update_student(student_update):
-    for student in students:
-        if student["id"] == student_update["id"]:
-            student.update(student_update)
-            return student
-    raise KeyError(f"student not found: {student_update['id']}")
+    return db.update_student(student_update)
 
 
 def delete_student(student_id):
-    for student in students:
-        if student["id"] == student_id:
-            students.remove(student)
-            return student
-    raise KeyError(f"Student with id {student_id} not deleted because not found: ")
+    return db.delete_student(student_id)
