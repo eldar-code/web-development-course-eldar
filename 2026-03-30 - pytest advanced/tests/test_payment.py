253cebd62a905995255b37b1a5055b66b36611b8
eldar-code <eldar.codementor@gmail.com>
2026-03-30 19:48:01 +0300

    practicing pytest

--- 2026-03-30 - pytest advanced/tests/test_payment.py
+++ 2026-03-30 - pytest advanced/tests/test_payment.py
@@ -1,27 +1,30 @@
+import pytest
+
+@pytest.mark.functional
 def test_authentication_positive():
     a = 2
     b = 3
     assert a + b == 5
 
-
+@pytest.mark.functional
 def test_authentication_negative():
     a = 2
     b = 3
     assert a + b == 5
 
-
+@pytest.mark.functional
 def test_authorization_positive():
     a = 2
     b = 3
     assert a + b == 5
 
-
+@pytest.mark.functional
 def test_authorization_negative():
     a = 2
     b = 3
     assert a + b == 5
 
-
+@pytest.mark.performance
 def test_performance():
     a = 2
     b = 3
